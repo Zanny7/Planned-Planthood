25321dbba2bcafa76b7917a9439a157d36a54959
Alex <olofalexander.larsson@gmail.com>
2026-01-02 11:46:30 +0100

    Resonsivity

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function shownav_ul_sidebar(){
+    const nav_ul_sidebar = document.querySelector('.nav_ul_sidebar')
+    nav_ul_sidebar.style.display = 'flex'
+    
+}
+
+
+function hidemenu() {
+    const menu=document.querySelector('.menu')
+    menu.style.display = 'none'
+}
+
+function showmenu() {
+    const menu=document.querySelector('.menu')
+    menu.style.display = 'flex'
+}
+
+function hidenav_ul_sidebar(){
+    const nav_ul_sidebar = document.querySelector('.nav_ul_sidebar')
+    nav_ul_sidebar.style.display = 'none'
+    
+}
